16d88d300a5d9f19899894e3158ceba83c9639af
cyberabis <babishek@gmail.com>
2015-10-14 01:27:44 +0530

    Added google analytics

--- js/cakebee.js
+++ js/cakebee.js
@@ -3,34 +3,33 @@ var myFirebaseRef = new Firebase("https://blazing-fire-9669.firebaseio.com/");
 
 $(document).ready(function() {
 
-	//Initially hide add another section
-	$('#add-another-section').hide();
+  	//Initially hide add another section
+  	$('#add-another-section').hide();
 
-  	myFirebaseRef.child('launch-timeline').on('value', function(snapshot) {
-	  //Update HTML span element launch-timeline
-	  $('#launch-timeline').text(snapshot.val());
-	});
+    	myFirebaseRef.child('launch-timeline').on('value', function(snapshot) {
+  	  //Update HTML span element launch-timeline
+  	  $('#launch-timeline').text(snapshot.val());
+  	});
 
-	//Preparing array of enabled dates
-	var twixRangeItr = moment.twix(new Date('2015-11-01'),new Date('2016-10-31')).iterate("days");
-	var enabledDatesArray =[];
-	while(twixRangeItr.hasNext()){
-	    enabledDatesArray.push(twixRangeItr.next().toDate());
-	}
-	
-	//Config for datepicker
+  	//Preparing array of enabled dates
+  	var twixRangeItr = moment.twix(new Date('2015-11-01'),new Date('2016-10-31')).iterate("days");
+  	var enabledDatesArray =[];
+  	while(twixRangeItr.hasNext()){
+  	    enabledDatesArray.push(twixRangeItr.next().toDate());
+  	}
+  	
+	  //Config for datepicker
   	$('#datetimepicker').datetimepicker({
 		format: 'DD-MM-YYYY',
         enabledDates: enabledDatesArray
     });
-    
 
   	//Pre-order button click logic
   	//1.Prevent default action for form submit
-	$("#preorder-form").submit(function(e){
-	    return false;
-	});
-	//2. Add submitted data to Firebase, clear and hide the section.
+  	$("#preorder-form").submit(function(e){
+  	    return false;
+  	});
+	  //2. Add submitted data to Firebase, clear and hide the section.
   	$('#preorder-button').click(function() {
   		var email = $('#email').val();
   		var mobilenumber= $('#mobilenumber').val();
@@ -50,14 +49,39 @@ $(document).ready(function() {
   		$( "#preorder-section" ).hide();
   		//show add-another section
   		$( "#add-another-section" ).show();
+
+      //For google analytics
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'preorder',
+        eventAction: 'submit',
+      });
   	});
   	
   	//Show preorder section
   	$('#add-another-btn').click(function() {
   		$( "#add-another-section" ).hide();
   		$( "#preorder-section" ).show();
+
+      //For google analytics 
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'preorder',
+        eventAction: 'add-another',
+      });
   	});
 
+
+    //Click event for website
+    $('.website-link').click(function() {
+      console.log('Website link clicked');
+      //For google analytics 
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'traffic',
+        eventAction: 'website-link-click',
+      });
+    });
     
 });
     
